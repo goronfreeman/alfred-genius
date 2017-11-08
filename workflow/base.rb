43897db3a80213f66219bb24b29efda588d09ad6
Hunter Braun <hunter.braun@gmail.com>
2017-11-07 19:30:59 -0600

    Add httparty

--- workflow/base.rb
+++ workflow/base.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH << 'lib/alfred-workflow-ruby'
 require 'alfred-3_workflow'
 
+$LOAD_PATH << 'lib/httparty'
+require 'httparty'
+
 $LOAD_PATH << 'lib/genius'
 require 'genius'
 
@@ -10,7 +13,7 @@ require 'yaml'
 module AlfredGenius
   module Base
     @@home_path = ENV['HOME']
-    @@dir_path = FileUtils.mkdir_p("#{@@home_path}/Library/Application Support/Alfred 3/Workflow Data/com.goronfreeman.genius.alfredworkflow")[0]
+    @@dir_path = FileUtils.mkdir_p("#{@@home_path}/Library/Application Support/Alfred 3/Workflow Data/com.goronfreeman.genius.alfredworkflow").first
     @@workflow = Alfred3::Workflow.new
   end
 end
